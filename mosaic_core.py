c8a8f5323528c8f134feec6aa68d3ee2bef8dcc1
Ankush Sharma <64264081+ankush290307@users.noreply.github.com>
2025-01-30 15:33:50 -0600

    Update mosaic_core.py

--- mosaic_core.py
+++ mosaic_core.py
@@ -1,1 +1,7 @@
-
+class PipelineOrchestrator:
+    """Detects available resources and optimizes workflow"""
+    
+    def __init__(self):
+        self.executor = ('nextflow' if shutil.which('nextflow') 
+                        else 'conda' if os.path.exists('envs/') 
+                        else 'bare-metal')
